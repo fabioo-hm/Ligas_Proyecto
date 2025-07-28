f50ad632d7c446de37733798baa89f332c2c7bf7
Fabio Andres Hernandez Manrique <194815533+fabioo-hm@users.noreply.github.com>
2025-07-28 12:30:24 -0500

    feat: :sparkles: Add new attributes

--- Models/Tournament.cs
+++ Models/Tournament.cs
@@ -11,16 +11,24 @@ public class Tournament
 
     public int Id { get; set; }
     public string? Name { get; set; }
+    public string? Country {get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public string? Type { get; set; }
 
-    public Tournament(int id, string? name)
+    public Tournament(int id, string? name, DateTime startd, DateTime endd, string? country, string? type)
     {
         Id = id;
         Name = name;
+        Country = country;
+        StartDate = startd;
+        EndDate = endd;
+        Type = type;
     }
     public Tournament() { }
     public override string ToString()
     {
-        return $"ID: {Id}, Torneo: {Name}";
+        return $"ID: {Id}, Torneo: {Name}, Pais: {Country}, Fecha de Inicio {StartDate.ToShortDateString()}, Fecha de Finalización: {EndDate.ToShortDateString()}";
     }
     public static void AddTournament(Tournament tournament)
     {
